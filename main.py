a45ae6be5596e9c925a72667793b39119eb4810c
zShan Ahmad <mahmad.zeeshan72@gmail.com>
2022-01-22 19:14:05 +0500

    changed heirarchy a little

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine
-from .routers import blog, user, authentication
+from src import models
+from src.database import engine
+from src.routers import blog, user, authentication
 
 # create all db tables if not already there
 models.Base.metadata.create_all(engine)
